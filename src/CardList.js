26a61f40a67e5a3a7d62c8d9da4616ddd5924be1
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-04-26 19:57:58 +0100

    addOthers

--- src/CardList.js
+++ src/CardList.js
@@ -2,13 +2,12 @@ import React from 'react'
 import Card from './card'
 
 function CardList({ robots }) {
-  return (
-    <div>
-      <Card name={robots[0].name} email={robots[0].email} id={robots[0].id} />
-      <Card name={robots[1].name} email={robots[1].email} id={robots[1].id} />
-      <Card name={robots[2].name} email={robots[2].email} id={robots[2].id} />
-    </div>
-  )
+  const cardComponent = robots.map((user, i) => {
+    return (
+      <Card name={robots[i].name} email={robots[i].email} id={robots[i].id} />
+    )
+  })
+  return <div>{cardComponent}</div>
 }
 
 export default CardList
